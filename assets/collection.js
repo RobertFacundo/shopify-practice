8eaa77185d14f7e3a5bdfa020b09d96c59ce6855
Facundo Robert <robert.facundo@hotmail.com>
2026-09-10 13:17:06 -0300

    feat: add collection filters & sorting

--- assets/collection.js
+++ assets/collection.js
@@ -1,3 +1,24 @@
+function updateProducts (url) {
+  return fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(html, 'text/html')
+
+      const products = doc.querySelector('.collection__products')
+
+      const currentProducts = document.querySelector('.collection__products')
+
+      currentProducts.replaceWith(products)
+    })
+}
+
+function getSectionId () {
+  const section = document.querySelector('.collection')
+  return section.dataset.sectionId
+}
+
+// Pagination
 document.addEventListener('click', event => {
   const link = event.target.closest('.collection__pagination a')
 
@@ -5,46 +26,81 @@ document.addEventListener('click', event => {
 
   event.preventDefault()
 
-  const section = document.querySelector('.collection')
-  const sectionId = section.dataset.sectionId
+  const sectionId = getSectionId()
 
   const url = `${link.href}&section_id=${sectionId}`
 
-  fetch(url)
-    .then(response => response.text())
-    .then(html => {
-      const parser = new DOMParser()
-      const doc = parser.parseFromString(html, 'text/html')
-
-      const products = doc.querySelector('.collection__products')
-
-      const currentProducts = document.querySelector('.collection__products')
-
-      currentProducts.replaceWith(products)
-
-      history.pushState({}, '', link.href)
-    })
+  updateProducts(url).then(() => {
+    history.pushState({}, '', link.href)
+  })
 })
 
+// Back / Forward
 window.addEventListener('popstate', () => {
-  const section = document.querySelector('.collection')
-  const sectionId = section.dataset.sectionId
+  const sectionId = getSectionId()
 
   const url = new URL(window.location.href)
   url.searchParams.set('section_id', sectionId)
-  console.log(window.location.href)
-  console.log(url.toString())
 
-  fetch(url)
-    .then(response => response.text())
-    .then(html => {
-      const parser = new DOMParser()
-      const doc = parser.parseFromString(html, 'text/html')
+  updateProducts(url)
+})
 
-      const products = doc.querySelector('.collection__products')
+// Filters
+document.addEventListener('change', event => {
+  const checkbox = event.target.closest(
+    '.collection__filters input[type="checkbox"]'
+  )
+  if (!checkbox) return
 
-      const currentProducts = document.querySelector('.collection__products')
+  const checkedCheckboxes = document.querySelectorAll(
+    '.collection__filters input[type="checkbox"]:checked'
+  )
 
-      currentProducts.replaceWith(products)
-    })
+  const params = new URLSearchParams()
+
+  checkedCheckboxes.forEach(checkbox => {
+    if (checkbox.name) {
+      params.append(checkbox.name, checkbox.value)
+    } else {
+      if (checkbox.dataset.min) {
+        params.append('filter.v.price.gte', checkbox.dataset.min)
+      }
+
+      if (checkbox.dataset.max) {
+        params.append('filter.v.price.lte', checkbox.dataset.max)
+      }
+    }
+  })
+
+  const url = new URL(window.location.href)
+  url.search = params
+  url.searchParams.delete('page')
+  url.searchParams.set('section_id', getSectionId())
+
+  const browserUrl = new URL(url)
+  browserUrl.searchParams.delete('section_id')
+
+  updateProducts(url).then(() => {
+    history.pushState({}, '', browserUrl)
+  })
+})
+
+//sort
+document.addEventListener('change', event => {
+  const select = event.target.closest('#SortBy')
+
+  if (!select) return
+
+  const url = new URL(window.location.href)
+
+  url.searchParams.set('sort_by', select.value)
+  url.searchParams.delete('page')
+  url.searchParams.set('section_id', getSectionId())
+
+  const browserUrl = new URL(url)
+  browserUrl.searchParams.delete('section_id')
+
+  updateProducts(url).then(() => {
+    history.pushState({}, '', browserUrl)
+  })
 })
